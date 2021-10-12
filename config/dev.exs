77c69f1d4ded2c2d9c07c6e5161fa662562651b3
Gustavo <g.92oliveira@gmail.com>
2021-10-11 21:26:55 -0300

    food order

--- config/dev.exs
+++ config/dev.exs
@@ -24,7 +24,15 @@ config :food_order, FoodOrderWeb.Endpoint,
   check_origin: false,
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
