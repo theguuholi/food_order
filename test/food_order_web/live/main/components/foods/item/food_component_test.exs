6e94fb1445ed95716e51ff65fc24dcc14a659061
Gustavo <g.92oliveira@gmail.com>
2021-10-15 06:40:26 -0300

    feat: validade item

--- test/food_order_web/live/main/components/foods/item/food_component_test.exs
+++ test/food_order_web/live/main/components/foods/item/food_component_test.exs
@@ -4,11 +4,21 @@ defmodule FoodOrderWeb.Main.Components.Foods.ItemComponentTest do
   import FoodOrder.Factory
 
   test "should load the load item information", %{conn: conn} do
-    insert(:product)
+    product = insert(:product)
+    product_2 = insert(:product)
     {:ok, view, _html} = live(conn, "/")
-    assert has_element?(view, "#food-item")
-    # assert is_there_id_with_text?(view, "#food-detail-text", "All Foods")
-    # assert has_element?(view, "#food-detail-products")
+    assert has_element?(view, "#food-item-#{product.id}")
+    assert has_element?(view, "#food-item-#{product_2.id}")
+  end
+
+  test "should load validade item details", %{conn: conn} do
+    product = insert(:product)
+    {:ok, view, _html} = live(conn, "/")
+    assert is_there_id_with_text?(view, "#food-item-name-#{product.id}", product.name)
+    assert is_there_id_with_text?(view, "#food-item-size-#{product.id}", product.size)
+    assert is_there_id_with_text?(view, "#food-item-size-#{product.id}", product.size)
+    assert is_there_id_with_text?(view, "#food-item-price-#{product.id}", Money.to_string(product.price))
+    assert is_there_id_with_text?(view, "#food-item-add-#{product.id}", "add")
   end
 
 end
