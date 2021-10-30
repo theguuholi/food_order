2a0880470ad3d74ef17e2923b1db52107240144f
Gustavo <g.92oliveira@gmail.com>
2021-10-30 07:07:09 -0300

    add session test

--- test/food_order/carts/boundaries/cart_session_api_test.exs
+++ test/food_order/carts/boundaries/cart_session_api_test.exs
@@ -18,4 +18,24 @@ defmodule FoodOrder.Carts.Boundaries.CartSessionApiTest do
     assert :ok == CartSessionApi.update(333, product)
     assert 1 == CartSessionApi.get(333).total_qty
   end
+
+  test "should add product and get" do
+    session_id = :rand.uniform(1000)
+
+    assert :ok == CartSessionApi.insert(session_id)
+    product = insert(:product, %{})
+    assert :ok == CartSessionApi.update(session_id, product)
+    assert :ok == CartSessionApi.add(session_id, product.id)
+    assert 2 == CartSessionApi.get(session_id).total_qty
+  end
+
+  test "should remove product and get" do
+    session_id = :rand.uniform(1000)
+
+    assert :ok == CartSessionApi.insert(session_id)
+    product = insert(:product, %{})
+    assert :ok == CartSessionApi.update(session_id, product)
+    assert :ok == CartSessionApi.remove(session_id, product.id)
+    assert 0 == CartSessionApi.get(session_id).total_qty
+  end
 end
