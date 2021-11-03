dc1f0a9ac19c64bfe51f6aa0ed0ba4ae2b935625
Gustavo <g.92oliveira@gmail.com>
2021-11-03 18:02:43 -0300

    prepare order a8: 19:29

--- lib/food_order_web/live/cart/components/order/confirm_order/confirm_order_component.ex
+++ lib/food_order_web/live/cart/components/order/confirm_order/confirm_order_component.ex
@@ -1,3 +1,25 @@
 defmodule FoodOrderWeb.Cart.Components.Order.ConfirmOrder.ConfirmOrderComponent do
   use FoodOrderWeb, :live_component
+  alias FoodOrder.Carts
+
+  def update(assigns, socket) do
+    {:ok, assign(socket, assigns)}
+  end
+
+  def handle_event("create_order", params, socket) do
+    # IO.inspect params
+    cart = Carts.get_cart(params["current_user"])
+    IO.inspect cart
+    # Order
+    # - belongs user
+    # - has many items
+    #   - item:
+    #     product_id
+    #     quantity
+    # - total_price
+    # - total_quantity:
+    # payment_type
+
+    {:noreply, socket}
+  end
 end
