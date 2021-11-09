772443ec775489be90996f3fd5b2ccfadfee93c7
Gustavo <g.92oliveira@gmail.com>
2021-11-09 07:03:33 -0300

    fix card test

--- test/food_order_web/live/admin/orders/order_layer_component/card_component/card_component_test.exs
+++ test/food_order_web/live/admin/orders/order_layer_component/card_component/card_component_test.exs
@@ -1,21 +1,30 @@
 defmodule FoodOrderWeb.Admin.CardComponentTest do
   use FoodOrderWeb.ConnCase
 
+  import FoodOrder.Factory
+  alias FoodOrder.Orders
   import Phoenix.LiveViewTest
 
+
   test "load card", %{conn: conn} do
+    insert(:order)
+
     {:ok, view, _html} = live(conn, "/admin/orders")
-    assert has_element?(view, "#card-pumpkin-1")
-    assert has_element?(view, "#card-pumpkin-1-order", "Order #123")
-    assert has_element?(view, "#card-pumpkin-1-order-items-title", "Order Items")
-    assert has_element?(view, "#card-pumpkin-1-order-items")
-    assert has_element?(view, "#card-pumpkin-1-order-item-1-name", "3 - Item 123")
-    assert has_element?(view, "#card-pumpkin-1-order-item-1-price", "$ 10")
-    assert has_element?(view, "#card-pumpkin-1-order-description", "Order Description")
-    assert has_element?(view, "#card-pumpkin-1-order-total-cost", "Total Cost:")
-    assert has_element?(view, "#card-pumpkin-1-order-total-amount", "$ 50")
-    assert has_element?(view, "#card-pumpkin-1-order-total-item", "Total Item:")
-    assert has_element?(view, "#card-pumpkin-1-order-total-item-amount", "4")
-    assert has_element?(view, "#card-pumpkin-1-order-customer-name", "Gustavo")
+
+    order = Orders.list_orders_by_status(:NOT_STARTED) |> hd
+
+    assert has_element?(view, "[data-role=card-board][data-id=NOT_STARTED-#{order.id}]")
+    assert has_element?(view, "[data-role=order-number][data-id=NOT_STARTED-#{order.id}]", order.id)
+    assert has_element?(view, "[data-role=order-items][data-id=NOT_STARTED]", "Order Items")
+    assert has_element?(view, "[data-role=items-list][data-id=NOT_STARTED]")
+    assert has_element?(view, "[data-role=item][data-id=NOT_STARTED-#{order.id}-#{hd(order.items).id}]")
+    assert has_element?(view, "[data-role=item-details][data-id=NOT_STARTED-#{order.id}-#{hd(order.items).id}]", "#{hd(order.items).quantity} - #{hd(order.items).product.name}")
+    assert has_element?(view, "[data-role=item-price][data-id=NOT_STARTED-#{order.id}-#{hd(order.items).id}]", "#{Money.to_string(hd(order.items).product.price)}")
+    assert has_element?(view, "[data-role=order-description-title]", "Order Description")
+    assert has_element?(view, "[data-role=total-cost]", "Total Cost:")
+    assert has_element?(view, "[data-role=total-cost-amount][data-id=NOT_STARTED-#{order.id}]", "#{Money.to_string(order.total_price)}")
+    assert has_element?(view, "[data-role=total-items]", "Total Item:")
+    assert has_element?(view, "[data-role=total-items-amount][data-id=NOT_STARTED-#{order.id}]", "#{order.total_quantity}")
+    assert has_element?(view, "[data-role=customer][data-id=NOT_STARTED-#{order.id}]", "#{order.user.email}")
   end
 end
