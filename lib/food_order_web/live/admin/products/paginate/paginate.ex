425d5ebc72353a427d0c94eddc76b936764a2f60
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-01-03 19:31:22 -0300

    include options

--- lib/food_order_web/live/admin/products/paginate/paginate.ex
+++ lib/food_order_web/live/admin/products/paginate/paginate.ex
@@ -1,9 +1,13 @@
 defmodule FoodOrderWeb.Admin.Products.Paginate do
   use FoodOrderWeb, :live_component
 
-  def pagination_link(%{page: page, text: text, socket: socket} = assigns) do
+  def pagination_link(%{page: page, options: options, text: text, socket: socket} = assigns) do
     ~H"""
-      <%= live_patch(text, to: Routes.admin_product_path(socket, :index, page: page)) %>
+      <%= live_patch(text, to: Routes.admin_product_path(socket, :index,
+                            page: page,
+                            per_page: options.per_page,
+                            sort_by: options.sort_by,
+                            sort_order: options.sort_order)) %>
     """
   end
 end
